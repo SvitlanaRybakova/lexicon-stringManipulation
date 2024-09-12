af3f36a6cec2558d129377263990acac4c8cd89b
Svitlana <rsv0011@gmail.com>
2024-09-12 16:48:07 +0200

    feat: implement the main and sinema menu

---
+++ stringManipulation/MenuHelpers.cs
@@ -0,0 +1,38 @@
+namespace stringManipulation
+{
+    public static class MenuHelpers
+    {
+
+        // the main menu references
+        public const string Quit = "0";
+        public const string SinemaMenu = "1";
+        public const string Repeat = "2";
+        public const string ThirdWord = "3";
+
+        // the sinema menu references
+        public const string SingleTicket = "1";
+        public const string GroupTicket = "2";
+
+
+
+        public static void ShowMainMenu()
+        {
+            Console.WriteLine("***** The Main Menu *****");
+            Console.WriteLine($"{Quit}. Exit the programm");
+            Console.WriteLine($"{SinemaMenu}. Display cinema tickets for various age groups and audience categories. ");
+            Console.WriteLine($"{Repeat}. Repeat the user input 10 times");
+            Console.WriteLine($"{ThirdWord}. Find the third word");
+
+        }
+
+        public static void ShowSinemaMenu()
+        {
+            Console.WriteLine("***** The Sinema Menu *****");
+            Console.WriteLine($"{Quit}. Exit the programm");
+            Console.WriteLine($"{SingleTicket}. Display the Single Ticket price ");
+            Console.WriteLine($"{GroupTicket}. Display the Group Ticket price");
+
+        }
+
+    }
+}
--- stringManipulation/Program.cs
+++ stringManipulation/Program.cs
@@ -1,2 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using stringManipulation;
+
+void RenderMainMenu()
+{
+    bool isOpen = true;
+
+    do
+    {
+
+        MenuHelpers.ShowMainMenu();
+
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case MenuHelpers.SinemaMenu:
+                RenderSinemaMenu();
+                break;
+            case MenuHelpers.Repeat:
+
+                break;
+            case MenuHelpers.ThirdWord:
+
+                break;
+            case MenuHelpers.Quit:
+                isOpen = false;
+                break;
+            default:
+                break;
+        }
+
+    } while (isOpen);
+}
+
+void RenderSinemaMenu()
+{
+
+    bool isOpen = true;
+
+    do
+    {
+        MenuHelpers.ShowSinemaMenu();
+
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case MenuHelpers.SingleTicket:
+                break;
+            case MenuHelpers.GroupTicket:
+                break;
+            case MenuHelpers.ThirdWord:
+                break;
+            case MenuHelpers.Quit:
+                isOpen = false;
+                break;
+            default:
+                break;
+        }
+
+    } while (isOpen);
+}
+
+RenderMainMenu();
